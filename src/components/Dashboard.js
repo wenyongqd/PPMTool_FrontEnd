ceb3bacda9dc6c925287f502098093754ac77448
wenyongqd <wenyongqd@gmail.com>
2020-03-30 20:08:50 -0400

    update fronten

---
+++ src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import ProjectItem from './Project/ProjectItem'
+
+export default class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome to the Dashboard</h1>
+                <ProjectItem />
+            </div>
+            
+        );
+    }
+}
---
+++ src/components/Project/ProjectItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ProjectItem extends Component {
+    render() {
+        return (
+            <div>
+                <h1>ProjectItem</h1>
+            </div>
+        )
+    }
+}
