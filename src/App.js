a5b4b67dddccbab0a05f46b7d5430abcd9da166c
wenyongqd <wenyongqd@gmail.com>
2020-05-22 21:21:12 -0400

    update

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React from 'react';
 import './App.css';
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import AddProject from './components/Project/AddProject';
+import { Provider } from 'react-redux'
+import store from './store'
+import UpdateProject from './components/Project/UpdateProject'
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Dashboard />
-    </div>
+    <Provider store={store}>
+      <Router>
+      <div className="App">
+        <Header />
+        {/* <Dashboard /> */}
+        <Route exact path="/addProject" component={AddProject}/>
+        <Route exact path="/dashboard" component={Dashboard}/>
+        <Route exact path="/updateProject/:id" component={UpdateProject}/>
+      </div>
+      </Router>
+    </Provider>
+    
+    
   );
 }
 
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,53 @@
 import React, { Component } from 'react'
 import ProjectItem from './Project/ProjectItem'
+import CreateProjectButton from './Project/CreateProjectButton';
+import { connect } from "react-redux"
+import { getProjects } from "../actions/projectActions"
+import PropTypes from "prop-types"
 
-export default class Dashboard extends Component {
+class Dashboard extends Component {
+
+    componentDidMount() {
+        this.props.getProjects()
+    }
+
     render() {
+
+        const {projects} = this.props.project
+
         return (
-            <div>
-                <h1 className="alert alert-warning">Welcome to the Dashboard</h1>
-                <ProjectItem />
+            <div className="projects">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <h1 className="display-4 text-center">Projects</h1>
+                    <br />
+                    <CreateProjectButton />
+                    <br />
+                    <hr />
+                    {/* Project Item Component */}
+                    {projects.map(project=>(
+                        <ProjectItem key={project.id} project={project}/>
+                    ))
+
+                    }
+                    {/* End of Project Item Component */}
+                </div>
             </div>
+        </div>
+    </div>
             
         );
     }
 }
+
+Dashboard.propTypes= {
+    project: PropTypes.object.isRequired,
+    getProjects: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+    project: state.project
+
+})
+export default connect(mapStateToProps, {getProjects })(Dashboard)
