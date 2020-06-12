a5f6350affd6d3e0c5755ab2d13f701930d07ab9
wenyongqd <wenyongqd@gmail.com>
2020-06-11 22:02:50 -0400

    update Login and Register

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ import UpdateProject from './components/Project/UpdateProject'
 import ProjectBoard from './components/ProjectBoard/ProjectBoard'
 import AddProjectTask from './components/ProjectBoard/ProjectTasks/AddProjectTask'
 import UpdateProjectTask from './components/ProjectBoard/ProjectTasks/UpdateProjectTask'
+import Landing from "./components/Layout/Landing"
+import Register from "./components/UserManagement/Register"
+import Login from "./components/UserManagement/Login"
 
 function App() {
   return (
@@ -18,6 +21,9 @@ function App() {
       <Router>
       <div className="App">
         <Header />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
         {/* <Dashboard /> */}
         <Route exact path="/addProject" component={AddProject}/>
         <Route exact path="/dashboard" component={Dashboard}/>
